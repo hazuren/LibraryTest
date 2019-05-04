c49a3a8f58e30f771b29ede33db5ca70a0e64201
PayDevD <mrpaydfordev@gmail.com>
2019-05-04 14:18:15 +0900

    add addBookTest method

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,5 +1,16 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class LibraryTest {
 
+    @Test
+    public void addBookTest() {
+        Library library = mock(Library.class);
+
+        library.addBook(new Book("The Hobbit", 304, "J.R.R. Tolkien"));
+        verify(library).addBook(new Book("The Hobbit", 304, "J.R.R. Tolkien"));
+    }
 }
