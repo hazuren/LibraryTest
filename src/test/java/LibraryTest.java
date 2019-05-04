d5a6726a4da439e407287c500019045cef59d602
PayDevD <mrpaydfordev@gmail.com>
2019-05-04 15:01:45 +0900

    add checkLibrarySizeTwice method

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.LinkedList;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -23,4 +25,17 @@ public class LibraryTest {
         verify(library).findByTitle(anyString());
     }
 
+    @Test
+    public void checkLibrarySizeTwice() {
+        Library library = new Library(new LinkedList<>());
+        Library librarySpy = spy(library);
+
+        librarySpy.addBook(new Book("Book1", 402, "John"));
+        librarySpy.addBook(new Book("Book2", 505, "Doe"));
+
+        int librarySizeTwice = librarySpy.size() + librarySpy.size();
+
+        assertThat(librarySizeTwice, is(4));
+        verify(librarySpy, times(2)).size();
+    }
 }
