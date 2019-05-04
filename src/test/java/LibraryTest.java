f1eb64a02a68ee8245c3139d551dd25f44052d67
PayDevD <mrpaydfordev@gmail.com>
2019-05-04 14:22:49 +0900

    add verifyFindByTitle test method

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,8 +1,8 @@
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class LibraryTest {
 
@@ -13,4 +13,14 @@ public class LibraryTest {
         library.addBook(new Book("The Hobbit", 304, "J.R.R. Tolkien"));
         verify(library).addBook(new Book("The Hobbit", 304, "J.R.R. Tolkien"));
     }
+
+    @Test
+    public void verifyFindByTitle() {
+        Library library = mock(Library.class);
+
+        when(library.findByTitle("The Hobbit")).thenReturn(new Book("The Hobbit", 304, "J.R.R. Tolkien"));
+        assertThat(library.findByTitle("The Hobbit"), is(new Book("The Hobbit", 304, "J.R.R. Tolkien")));
+        verify(library).findByTitle(anyString());
+    }
+
 }
